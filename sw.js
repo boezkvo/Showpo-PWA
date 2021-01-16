eb52f2513c8a2462ef5e150d2ab2df40c6129d88
boezkvo <homalayan_aj@yahoo.com>
2021-01-16 08:27:32 +0800

    Initial Commit

---
+++ sw.js
@@ -0,0 +1,12 @@
+// install event
+self.addEventListener('install', evt => {
+  console.log('service worker installed');
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  console.log('service worker activated');
+});
+
+this.addEventListener('fetch', function (event) {
+});
